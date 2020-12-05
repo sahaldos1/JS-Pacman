9f2ec607436f3d2c82d6617720141a3bd9fded11
Sahal Asghar <sahaldos1@gmail.com>
2020-12-04 21:44:47 -0500

    remove excess comments

--- Pacman.js
+++ Pacman.js
@@ -1,14 +1,3 @@
-// class Pacman {
-//   //constructor for pacman
-//   constructor(speed, startPos) {
-//     //start pos is just where pacman starts in the grid and speed is how fast he will move
-//     this.pos = startPos;
-//     this.speed = speed;
-//   }
-// }
-
-// export default Pacman;
-
 import { OBJECT_TYPE, DIRECTIONS } from "./basics";
 
 class Pacman {
